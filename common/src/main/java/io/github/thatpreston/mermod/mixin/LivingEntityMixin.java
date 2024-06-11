b6388da0a0243462d085d734ca2e714a936bde18
ThatPreston <thatprestonsecure@gmail.com>
2024-06-11 18:24:43 -0500

    Use ModifyExpressionValue instead of ModifyVariable

--- common/src/main/java/io/github/thatpreston/mermod/mixin/LivingEntityMixin.java
+++ common/src/main/java/io/github/thatpreston/mermod/mixin/LivingEntityMixin.java
@@ -1,16 +1,16 @@
 package io.github.thatpreston.mermod.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import io.github.thatpreston.mermod.Mermod;
 import io.github.thatpreston.mermod.config.MermodConfig;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-    @ModifyVariable(method = "travel(Lnet/minecraft/world/phys/Vec3;)V", at = @At(value = "STORE", ordinal = 0), ordinal = 1)
+    @ModifyExpressionValue(method = "travel(Lnet/minecraft/world/phys/Vec3;)V", at = @At(value = "CONSTANT", args = "floatValue=0.02F", ordinal = 0))
     private float modifyFloat(float original) {
         if((Object)this instanceof Player player) {
             if(!Mermod.getNecklace(player).isEmpty()) {
