ea67e423b3b9b903881e87c9b65658b64d090eac
ThatPreston <thatprestonsecure@gmail.com>
2025-09-06 18:45:39 -0500

    Fix aqua affinity config option

--- fabric/src/main/java/io/github/thatpreston/mermod/fabric/mixin/PlayerMixin.java
+++ fabric/src/main/java/io/github/thatpreston/mermod/fabric/mixin/PlayerMixin.java
@@ -2,6 +2,7 @@ package io.github.thatpreston.mermod.fabric.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import io.github.thatpreston.mermod.Mermod;
+import io.github.thatpreston.mermod.config.MermodConfig;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 public abstract class PlayerMixin {
     @ModifyExpressionValue(method = "getDestroySpeed(Lnet/minecraft/world/level/block/state/BlockState;)F", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/attributes/AttributeInstance;getValue()D"))
     private double getSubmergedMiningSpeed(double original) {
-        if((Object)this instanceof Player player) {
+        if(MermodConfig.getAquaAffinity() && (Object)this instanceof Player player) {
             if(!Mermod.getNecklace(player).isEmpty()) {
                 return Math.max(1, original);
             }
